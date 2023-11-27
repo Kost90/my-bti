a7207356022eb810d1d1c0f3ef68728871d53823
kost90 <kostdonc90@gmail.com>
2023-11-27 19:42:36 +0000

    edited calendar component and TimePicker component

--- src/components/calendar/Calendar.tsx
+++ src/components/calendar/Calendar.tsx
@@ -2,21 +2,19 @@
 import { useState, useEffect } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-// import TimePicker from "./TimePicker";
+import TimePicker from "./TimePicker";
 import { isWeekdayAndPassday } from "./helpers";
 
-type bookedArr = {
+export type bookedArr = {
   date: string;
   times: number[];
 };
 
 export default function Calendar() {
-  const [currentDate, setcurrentDate] = useState<string>();
+  const [currentDate, setcurrentDate] = useState<string>("");
   const [dates, setDates] = useState<bookedArr[]>([]);
-  const [dateObj, setDateObj] = useState<Date[]>([]);
   const [time, setTime] = useState<number[]>([]);
-  const [excludedTimes, setExcludedTimes] = useState<Date[]>([]);
-
+  console.log("date component");
   useEffect(() => {
     if (currentDate) {
       const index = dates.findIndex((item) => item.date === currentDate);
@@ -43,33 +41,8 @@ export default function Calendar() {
     }
   }, [time]);
 
-  useEffect(() => {
-    if (currentDate) {
-      const selectedDate = new Date(currentDate);
-      const excludedDateTimes = dateObj.filter(
-        (dateTime) => dateTime.toDateString() === selectedDate.toDateString()
-      );
-      setExcludedTimes(excludedDateTimes);
-
-    }
-  }, [currentDate, dateObj]);
-
-  useEffect(() => {
-    const newDateObjects: Date[] = [];
-
-    dates.forEach((item) => {
-      item.times.forEach((time) => {
-        const dateString = `${item.date} ${time}:00`;
-        newDateObjects.push(new Date(dateString));
-      });
-    });
-
-    setDateObj(newDateObjects);
-  }, [dates]);
-
   const handelAddDate = (data: Date) => {
     const newDate = data.toISOString().split("T")[0];
-
     setcurrentDate(newDate);
   };
 
@@ -82,28 +55,18 @@ export default function Calendar() {
     <div>
       <h4>Оберіть дату виїзду:</h4>
       <DatePicker
-        // filterDate={(date) => !excludedDates.includes(date.toISOString().split("T")[0])}
         filterDate={isWeekdayAndPassday}
-        // selected={new Date()}
         onChange={handelAddDate}
         dateFormat={"dd.MM.yyyy"}
-        // excludeDates={dateObj}
         timeIntervals={120}
-        // filterTime={filterTime}
         placeholderText="Оберіть свободну дату"
       />
-      <DatePicker
+      <TimePicker
+        time={time}
+        dates={dates}
+        curentDate={currentDate}
         onChange={handelAddTime}
-        showTimeSelect
-        showTimeSelectOnly
-        timeIntervals={120}
-        excludeTimes={excludedTimes}
-        timeCaption="Time"
-        dateFormat="h:mm aa"
-        minTime={new Date(0, 0, 0, 9, 0)}
-        maxTime={new Date(0, 0, 0, 18, 0)}
       />
-      {/* <TimePicker onChange={handelChange} data={dates} /> */}
     </div>
   );
 }
--- src/components/calendar/TimePicker.tsx
+++ src/components/calendar/TimePicker.tsx
@@ -1,31 +1,59 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import type { bookedArr } from "./Calendar";
 
 interface TimePickerProps {
-  onChange: (hours: number, minutes: number) => void;
-  data: Date[];
+  time: number[];
+  dates: bookedArr[];
+  curentDate: string;
+  onChange: (data: Date) => void;
 }
 
-function TimePicker({ onChange, data }: TimePickerProps) {
-  const handelAddTime = (time: Date) => {
-    const dateTime = time;
-    const hours = dateTime.getHours();
-    const minutes = dateTime.getMinutes();
-    onChange(hours, minutes);
-  };
+function TimePicker({ dates, curentDate, onChange }: TimePickerProps) {
+  const [dateObj, setDateObj] = useState<Date[]>([]);
+  const [excludedTimes, setExcludedTimes] = useState<Date[]>([]);
 
+  console.log("time component");
+  useEffect(() => {
+    if (curentDate) {
+      const selectedDate = new Date(curentDate);
+      const excludedDateTimes = dateObj.filter(
+        (dateTime) => dateTime.toDateString() === selectedDate.toDateString()
+      );
+      setExcludedTimes(excludedDateTimes);
+    }
+  }, [curentDate, dateObj]);
+
+  useEffect(() => {
+    const newDateObjects: Date[] = [];
+
+    dates.forEach((item) => {
+      item.times.forEach((time) => {
+        const dateString = `${item.date} ${time}:00`;
+        newDateObjects.push(new Date(dateString));
+      });
+    });
+
+    setDateObj(newDateObjects);
+  }, [dates]);
 
   return (
-    <DatePicker
-      onChange={handelAddTime}
-      showTimeSelect
-      showTimeSelectOnly
-      timeIntervals={120}
-      timeCaption="Time"
-      dateFormat="h:mm aa"
-    />
+    <>
+      <h4>Обрати час:</h4>
+      <DatePicker
+        onChange={onChange}
+        showTimeSelect
+        showTimeSelectOnly
+        timeIntervals={120}
+        excludeTimes={excludedTimes}
+        timeCaption="Time"
+        dateFormat="h:mm aa"
+        minTime={new Date(0, 0, 0, 9, 0)}
+        maxTime={new Date(0, 0, 0, 18, 0)}
+      />
+    </>
   );
 }
 
