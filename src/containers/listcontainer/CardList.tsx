f0f6de66ea7c15aa98364dac63238699c71b46c8
kost90 <kostdonc90@gmail.com>
2023-11-30 20:27:02 +0000

    added animation for cards section, edited carlist component, deleted card component

--- src/containers/listcontainer/CardList.tsx
+++ src/containers/listcontainer/CardList.tsx
@@ -1,25 +1,101 @@
-import { Box } from "@mui/material";
-import Card from "../../components/cards/Card";
+"use client";
+import { useState } from "react";
 import { cardContent } from "@/constants/CardsData";
-import { flexContainer } from "../../components/cards/stylesconstans";
+import { motion, AnimatePresence } from "framer-motion";
 import styles from "./CardList.module.css";
+import Link from "next/link";
+import Image from "next/image";
 
-const CardList = async () => {
+const CardList = () => {
+  const [selectedId, setSelectedId] = useState("");
+
   return (
-    <Box>
-      <h4 className={styles.h4}>НАШІ ПОСЛУГИ:</h4>
-      <Box sx={flexContainer}>
-        {cardContent.map((item, i) => (
-          <Card
-            key={i}
-            title={item.title}
-            services={item.services}
-            path={item.path}
-            src={item.src}
-          />
+    <motion.div>
+      <motion.h4
+        initial={{ opacity: 0, x: -100 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.3 }}
+        viewport={{ once: true }}
+        className={styles.h4}
+      >
+        НАШІ ПОСЛУГИ:
+      </motion.h4>
+      <motion.div
+        initial={{ opacity: 0, y: -100 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+        viewport={{ once: true }}
+        className={styles.grid_container}
+      >
+        {cardContent.map((item) => (
+          <motion.div
+            className={`${styles.card_container} ${
+              selectedId === item.id ? "card-selected" : ""
+            }`}
+            layoutId={`card-container-${item.id}`}
+            onClick={() => setSelectedId(item.id)}
+            key={item.id}
+            initial={{ scale: 1 }}
+            animate={{ scale: selectedId === item.id ? 1.1 : 1 }}
+            transition={{ duration: 0.3 }}
+          >
+            <div className={styles.card_content}>
+              <Image src={item.src} alt="icon" />
+              <motion.h2>{item.title}</motion.h2>
+            </div>
+          </motion.div>
         ))}
-      </Box>
-    </Box>
+      </motion.div>
+
+      <AnimatePresence>
+        {selectedId && (
+          <motion.div
+            className={styles.selected_card_container}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            {cardContent.map(
+              (item) =>
+                item.id === selectedId && (
+                  <motion.div
+                    className={styles.selected_card}
+                    layoutId={`card-container-${item.id}`}
+                    key={item.id}
+                    initial={{ scale: 0.8, opacity: 0 }}
+                    animate={{ scale: 1, opacity: 1 }}
+                    exit={{ scale: 0.8, opacity: 0 }}
+                  >
+                    <motion.div className={styles.relative}>
+                      <div className={styles.container_close_btn}>
+                        <Image src={item.src} alt="icon" />
+                        <button
+                          className={styles.close_btn}
+                          onClick={() => setSelectedId("")}
+                        >
+                          X
+                        </button>
+                      </div>
+
+                      <motion.h2>{item.title}</motion.h2>
+                      <motion.ul>
+                        {item.services.map((item, i) => (
+                          <li key={i}>{item}</li>
+                        ))}
+                      </motion.ul>
+                      <button type="button">
+                        <Link href={item.path} key={item.id}>
+                          Детальніше
+                        </Link>
+                      </button>
+                    </motion.div>
+                  </motion.div>
+                )
+            )}
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </motion.div>
   );
 };
 
